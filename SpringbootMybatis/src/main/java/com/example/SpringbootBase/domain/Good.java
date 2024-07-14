5109dd48dc7fbbe52586968cabddc7110f212c47
XuHaijwill <zuiwoxuanlou@163.com>
2024-07-14 20:38:52 +0800

    mybatis commit

---
+++ SpringbootMybatis/src/main/java/com/example/SpringbootBase/domain/Good.java
@@ -0,0 +1,18 @@
+package com.example.SpringbootBase.domain;
+
+import lombok.Data;
+
+/**
+ * @ClassName Good
+ * @Description //TODO
+ * @Author XuHaijwill
+ * @Date 2024/7/11 8:54
+ * @Version 1.0
+ **/
+@Data
+public class Good {
+    private String name;
+    private String price;
+    private String color;
+    private String time;
+}
---
+++ SpringbootMybatis/src/test/java/com/example/SpringbootBase/SpringbootBaseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.SpringbootBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hello word");
+	}
+
+}
