7524e6086ff4f2db4e43b45bb7eb89f7d77f24f6
XuHaijwill <zuiwoxuanlou@163.com>
2024-08-16 20:57:30 +0800

    poi

---
+++ SpringbootPDF/src/test/java/com/example/SpringbootBaseMaven/SpringbootPDFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootBaseMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPDFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
